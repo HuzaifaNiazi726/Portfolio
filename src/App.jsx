5e09445099963f55d9bb1a6610471c06dc71cd7b
Huzaifa Niazi <HuzaifaNiazi726@gmail.com>
2026-02-02 22:40:32 -0800

    Removed Home and CardLanding components

--- src/App.jsx
+++ src/App.jsx
@@ -5,25 +5,23 @@ import './App.css'
 import {  BrowserRouter,Route,Routes} from 'react-router-dom'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Portfolio from './components/Portfolio';
-import Landing from './components/Landing'
-import Home1 from './components/Home1'
-import Footer from './components/Footer'  
 
-import Navigation from './components/Navigation'
+
+
 
 function App() {
   
 
   return (
    <>
-   <BrowserRouter>
+   
 <Portfolio/>
    
 
 
     
     
-    </BrowserRouter>
+    
    
   
   </>
